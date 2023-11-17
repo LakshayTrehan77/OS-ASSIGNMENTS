0797d0d25093650e56d280529121613eb28ff9b8
LakshayTrehan77 <133641907+LakshayTrehan77@users.noreply.github.com>
2023-11-17 18:52:35 +0530

    Update Bridge.c

--- OS-Assignment-4/Q3/Bridge.c
+++ OS-Assignment-4/Q3/Bridge.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <bits/getopt_core.h>
+#include <semaphore.h>
+
 #define EAST 1
 #define WEST 2
 
@@ -10,42 +11,59 @@ typedef struct bridge
 {
     int cars;
     int direction;
-    pthread_mutex_t mutex;
-    pthread_cond_t empty;
+    sem_t mutex;
+    sem_t max_cars_sem;
 } bridge_t;
 
 bridge_t shared_bridge = {
-.cars = 0,
-.direction = WEST,
-.mutex = PTHREAD_MUTEX_INITIALIZER,
-.empty = PTHREAD_COND_INITIALIZER,
+    .cars = 0,
+    .direction = WEST,
+    .mutex = PTHREAD_MUTEX_INITIALIZER,
+    .max_cars_sem = PTHREAD_MUTEX_INITIALIZER,
 };
 
 void arrive(bridge_t *bridge, int direction)
 {
-    pthread_mutex_lock(&bridge->mutex);
-    while (bridge->cars > 0 && (bridge->cars > 2 || bridge->direction != direction))pthread_cond_wait(&bridge->empty, &bridge->mutex);
+    sem_wait(&bridge->mutex);
+
+    while (bridge->cars > 0 && (bridge->cars > 2 || bridge->direction != direction))
+    {
+        sem_post(&bridge->mutex);
+        sem_wait(&bridge->mutex);
+    }
+
     if (bridge->cars == 0)
         bridge->direction = direction;
+
     bridge->cars++;
-    pthread_mutex_unlock(&bridge->mutex);
+    sem_post(&bridge->mutex);
 }
 
 void cross(bridge_t *bridge)
 {
-    if(bridge->direction == 1)
+    sem_wait(&bridge->max_cars_sem); // Wait if maximum cars are already on the bridge
+
+    sem_wait(&bridge->mutex);
+
+    if (bridge->direction == 1)
         printf("Crossing East ... number of Cars on bridge = %d \n", bridge->cars);
     else
         printf("Crossing West ... number of Cars on bridge = %d \n", bridge->cars);
+
+    sem_post(&bridge->mutex);
+
     sleep(1);
+
+    sem_post(&bridge->max_cars_sem); // Release the semaphore after crossing
 }
 
 void leave(bridge_t *bridge)
 {
-    pthread_mutex_lock(&bridge->mutex);
+    sem_wait(&bridge->mutex);
+
     bridge->cars--;
-    pthread_cond_signal(&bridge->empty);
-    pthread_mutex_unlock(&bridge->mutex);
+
+    sem_post(&bridge->mutex);
 }
 
 void drive(bridge_t *bridge, int direction)
@@ -55,15 +73,15 @@ void drive(bridge_t *bridge, int direction)
     leave(bridge);
 }
 
-void* east(void *data)
+void *east(void *data)
 {
-    drive((bridge_t *) data, EAST);
+    drive((bridge_t *)data, EAST);
     return NULL;
 }
 
-void* west(void *data)
+void *west(void *data)
 {
-    drive((bridge_t *) data, WEST);
+    drive((bridge_t *)data, WEST);
     return NULL;
 }
 
@@ -71,15 +89,24 @@ int run(int nw, int ne)
 {
     int i, n = nw + ne;
     pthread_t thread[n];
+
+    sem_init(&shared_bridge.mutex, 0, 1);
+    sem_init(&shared_bridge.max_cars_sem, 0, 5);
+
     for (i = 0; i < n; i++)
-    if (pthread_create(&thread[i], NULL, i < nw ? east : west, &shared_bridge))
-    {
-        printf("thread creation failed\n");
-        return EXIT_FAILURE;
-    }
+        if (pthread_create(&thread[i], NULL, i < nw ? east : west, &shared_bridge))
+        {
+            printf("thread creation failed\n");
+            return EXIT_FAILURE;
+        }
+
     for (i = 0; i < n; i++)
         if (thread[i])
             pthread_join(thread[i], NULL);
+
+    sem_destroy(&shared_bridge.mutex);
+    sem_destroy(&shared_bridge.max_cars_sem);
+
     return EXIT_SUCCESS;
 }
 
@@ -94,21 +121,23 @@ int main(int argc, char **argv)
     {
         switch (c)
         {
-            case 'e':
-                if ((ne = atoi(optarg)) <= 0){
+        case 'e':
+            if ((ne = atoi(optarg)) <= 0)
+            {
                 fprintf(stderr, "Number of cars going east must be greater than 0\n");
                 exit(EXIT_FAILURE);
-                }
-                break;
-            case 'w':
-                if ((nw = atoi(optarg)) <= 0) {
+            }
+            break;
+        case 'w':
+            if ((nw = atoi(optarg)) <= 0)
+            {
                 fprintf(stderr, "Number of cars going west must be greater than 0\n");
                 exit(EXIT_FAILURE);
-                }
-                break;
-            case 'h':
-                printf("Usage: bridge [-e east] [-w west] [-h]\n");
-                exit(EXIT_SUCCESS);
+            }
+            break;
+        case 'h':
+            printf("Usage: bridge [-e east] [-w west] [-h]\n");
+            exit(EXIT_SUCCESS);
         }
     }
     return run(nw, ne);
